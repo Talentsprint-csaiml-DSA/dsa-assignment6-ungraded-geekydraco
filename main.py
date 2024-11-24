684713b57383fda1ffe55735ec50ec1477665d1c
geekydraco <anjanrockz@live.com>
2024-11-25 00:53:12 +0530

    done

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-def longest_box_sequence(boxes):
-    #ToDo 
-    #Write your code here.
-    
+def can_pack(box_a, box_b):
+    return all(a < b for a, b in zip(box_a, box_b))
+
+def box_packing(boxes):
+    boxes.sort(key=lambda x: (x[0], x[1], x[2]))
+
+    n = len(boxes)
+    dp = [1] * n  
+
+    for i in range(n):
+        for j in range(i):
+            if can_pack(boxes[j], boxes[i]):
+                dp[i] = max(dp[i], dp[j] + 1)
+
+    return max(dp) 
